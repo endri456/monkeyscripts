2caed0bab771a1f5c67dec79edf36c08c8621dd0
noc-sup <noc-sup@noc2.internal.labs.it>
2023-07-05 13:47:20 +0200

    RT - WIP v1.2

--- rt-scripts/rt-wip.js
+++ rt-scripts/rt-wip.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name RT - WIP
 // @description Script RT
-// @version 1.1
+// @version 1.2
 // @grant none
 // @match https://rt.labs.it/rt/Dashboards/41326/*
 // ==/UserScript==
